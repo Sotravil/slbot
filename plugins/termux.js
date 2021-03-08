1bded4334b0849ce4b47ee50000b13bcfb27d046
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-07 18:49:30 -0600

    Add files via upload

---
+++ plugins/termux.js
@@ -0,0 +1,137 @@
+exports.termux = termux()
+function termux() {
+	return `*COMANDOS PARA TERMUX*
+_CAMBIAR BANER_
+pkg install cowsay
+pkg install toilet
+apt list
+pkg update yy apk upgrade
+pkg install git
+git clone https://github.com/Bhai4You/Termux-Banner
+ls
+cd Termux-Banner
+ls
+cd bash requierement.sh
+bash t-ban.sh
+---------------------------------------------
+pkg install neofetch -y
+ls
+cd ..
+ls
+cd usr
+ls
+cd etc
+ls
+pkg install nano
+ls
+rm motd
+ls
+nano bash.bash
+PS1 (name:)
+echo -e "\e[1;32m"
+neofetch
+echo ""
+ctrl-x-y
+---------------------------------------------
+_HACK CAMERA_
+apt-get install python3
+apt-get install git
+git clone https://github.com/AngelesecurityTeam/Cam-Hackers
+pip3 istall requests
+ls
+cd Cam-Hackers
+python3 cam-hackers.py
+---------------------------------------------
+git clone https://github.com/thelinuxchoice/saycheese
+ls
+cd saycheese
+ls
+chmod 777*
+ls
+ls
+bash saycheese.sh
+1
+y
+internetgratis
+ctrl C
+ls
+mv .png .png .png sdcard
+---------------------------------------------
+git clone https://github.com/kancotdiq/ipcs
+pip2 install requests
+pip2 install --upgrade pip cd ipcs
+ls
+cd ipcs
+ls
+python2 scam.py
+123
+---------------------------------------------
+_FUERZA BRUTA_
+apt install -y python2-dev
+apt install pip2
+pip2 install mechanize
+git clone https://github.com/perjayro/facebook_brute.git
+cd Facebook_brute
+chmod +x brute.py
+python2 brute.py
+ID
+sellect diccionario
+---------------------------------------------
+ pip2 install --upgrade pip
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+cd hack-facebook-teman
+sh requests.sh
+python2 hack-fb.py
+---------------------------------------------
+_PHISHING_
+pkg install python (2 y 3)
+pkg install wget
+pkg install git
+git clone https://github.com/AngelSecurityTeam/Recreator-Phishing.git
+ls
+cd Recreator-phishing
+ls
+python2 recreator-phishing.py
+ls
+bash install.sh
+python3 recreator-phishing.py
+---------------------------------------------
+pkg install php
+git clone http://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+chmod +x*
+bash blackeye.sh
+****elige un numero****
+usage: hostname [OPTIONS] [HOSTNAME │ -F FILE]
+---------------------------------------------
+apt install wget curl php
+pkg install bash 777
+git clone https://github.com/Cesar-Hack-Gray/scam.git
+ls
+cd scam
+ls
+chmod 777 install.sh
+ls
+bash install.sh
+./phishing.sh
+---------------------------------------------
+*SUSCRIBETE A MI CANAL!!:)*
+---------------------------------------------
+_HACK WIFI_
+pip 2 install
+git clone https://github.com/threat9/routersploit
+ls
+cd routersploit
+ls
+pip2 install -r requirements -dev.txt
+---------si hay error----------
+      pip install future
+----------------------------------
+ls
+python rsf.py
+---------------------------------------------
+©Licensed Samu330®
+ █║▌│█│║▌║││█║▌║`
+}
