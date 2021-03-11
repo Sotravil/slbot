ff9e46c270c93e9851e29b045855a5071dca8712
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-11 14:56:56 -0600

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    global.DATABASE._data.users[m.sender].exp += 570
-    m.reply('+500 XP')
+    global.DATABASE._data.users[m.sender].exp += 700
+    m.reply('+700 XP')
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
   } else m.reply('Ya has reclamado el dia de hoy')
 }
-handler.help = ['daily', 'claim']
+handler.help = ['d', 'daily', 'claim']
 handler.tags = ['xp']
 handler.command = /^(daily|claim)$/i
 handler.owner = false
