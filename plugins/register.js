b05bb2df06e69a73832010c7e9d6947ace5e6cb0
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-11 10:14:36 -0600

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -3,7 +3,7 @@ let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
   if (user.registered === true) throw `Usted ya está registrado\nMau reinscripción? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Formato incorrecto\n*${usedPrefix}registrar <nombre>.edad>*`
+  if (!Reg.test(text)) throw `Formato incorrecto\n*para registrarse coloque ${usedPrefix}registrar <nombre>.edad>*`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'El nombre no puede estar vacío (alfanumérico)'
   if (!age) throw 'La edad no puede estar en blanco (Número)'
@@ -23,7 +23,7 @@ let handler = async function (m, { text, usedPrefix }) {
 ╰────
 `.trim())
 }
-handler.help = ['rg', 'registrar', 'reg', 'register'].map(v => v + 'para registrarse coloque <nombre>.<edad>')
+handler.help = ['rg', 'registrar', 'reg', 'register'].map(v => v + '<nombre>.<edad>')
 handler.tags = ['exp']
 
 handler.command = /^(registrarse|reg(ister)?)$/i
