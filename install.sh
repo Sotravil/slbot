63272c5fb8b4c0aca7c05de3d723ac27e2e2260b
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-12-12 10:26:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-pkg install nodejs -y
-pkg install ffmpeg -y
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 pkg install imagemagick -y
 npm install
+
+echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
